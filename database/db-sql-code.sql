6b985d5754d408246353ddedf9c6d364ae9b5dfd
Igor2games <igor1games@outlook.com>
2026-03-13 23:56:53 -0300

    sql

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -236,3 +236,15 @@ VALUES   (
     'White',
     5
   );
+
+--4
+UPDATE  inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'huge interior')
+WHERE inv_id = '10';
+
+--6
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/')
+WHERE inv_image LIKE '/images/%'
+    AND inv_thumbnail LIKE '/images/%';
