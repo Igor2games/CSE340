c5266c73e2c0e0fffb91e925fcd6cd7a5cee6740
Igor2games <igor1games@outlook.com>
2026-03-25 19:46:45 -0300

    account register view built

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,6 +7,10 @@ const utilities = require("../utilities")
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
+// Route to build login view
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
+
 // Intentional error route for testing error handling middleware
 router.get("/trigger-error", utilities.handleErrors(accountController.triggerIntentionalError));
 
