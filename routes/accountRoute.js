150e33fe3d309041ae889bc53af78dab96fdbf7a
Igor2games <igor1games@outlook.com>
2026-03-31 22:34:55 -0300

    login / cookies added

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,6 +5,9 @@ const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
 
+// Route to build account management view
+router.get("/", utilities.handleErrors(accountController.buildManagement));
+
 // Route to build login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
@@ -23,14 +26,12 @@ router.post(
 // Intentional error route for testing error handling middleware
 router.get("/trigger-error", utilities.handleErrors(accountController.triggerIntentionalError));
 
-// Process the login attempt
+// Process the login request
 router.post(
   "/login",
   regValidate.loginRules(),
   regValidate.checkLoginData,
-  (req, res) => {
-    res.status(200).send('login process')
-  }
+  utilities.handleErrors(accountController.accountLogin)
 )
 
 module.exports = router;
